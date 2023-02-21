81346a7851f31cc13441b5144ee79dd06e829607
Seungjun <seungjun.chae@stonybrook.edu>
2023-02-20 23:05:40 -0500

    created basic route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+// ROUTE routers
+const indexRouter = require('./routes/index');
+
 dotenv.config();
 app.use(express.json());
 
@@ -16,6 +19,8 @@ const db = mongoose.connection;
 
 db.once('open', () => console.log('Connected to MongoDB'));
 db.on('error', (err) => console.error('connection error:', err));
+
+app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3666, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
