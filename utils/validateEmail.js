30d9db17f1b714d50dc92a2a03d5b1a1d95ec792
Seungjun <seungjun.chae@stonybrook.edu>
2023-03-21 02:33:43 -0400

    updated regex following Google Account validation rule

--- utils/validateEmail.js
+++ utils/validateEmail.js
@@ -1,6 +1,7 @@
-function validateEmail(email){
-  const regex = /^\w+([\.-]?\w+)*@gmail\.com$/;
-  return regex.test(email);
+function validateEmail(email) {
+	// const regex = /^\w+([\.-]?\w+)*@gmail\.com$/;
+	const regex = /^[a-zA-Z0-9\.]{6,30}@gmail\.com$/;
+	return regex.test(email);
 }
 
 module.exports = validateEmail;
