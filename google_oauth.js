ba6602235a17e0c4b9a69c521d67f287674b8958
mlee990125 <michael.j.lee@stonybrook.edu>
2023-03-28 11:35:53 -0400

    added port number to env file in google_oauth.js

--- google_oauth.js
+++ google_oauth.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3333/auth/google/callback",
+  callbackURL: `http://localhost:${process.env.PORT}/auth/google/callback`,
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
