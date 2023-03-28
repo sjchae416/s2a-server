95b7d25fdf2c83bea840c19477a18dfc51f421f4
Seungjun Chae <seungjun.chae@stonybrook.edu>
2023-03-27 20:12:32 -0400

    fixed app-specific and table-specific Views deletion & tested successfully

--- routes/apps.js
+++ routes/apps.js
@@ -25,25 +25,34 @@ const validateEmail = require('../utils/validateEmail');
 
 // ROUTE Crud - create an App
 router.post('/', async (req, res) => {
-	// REVIEW deleted dataSources and views becuase the client will not be able to pass at this point
-	const { name, creator, roles, roleMembershipSheet, published } = req.body;
+	const {
+		name,
+		creator,
+		tables,
+		views,
+		roles,
+		roleMembershipSheet,
+		published,
+	} = req.body;
 
 	try {
 		const newApp = await App.create({
 			name: name,
 			creator: creator,
+			tables: tables,
+			views: views,
 			roles: roles,
 			roleMembershipSheet: roleMembershipSheet,
 			published: published,
 		});
 
-		//Iterates through the array of values under each key in the roles object.
+		// NOTE Iterates through the array of values under each key in the roles object.
 		for (let key in newApp.roles) {
 			for (let i = 0; i < newApp.roles[key].length; i++) {
 				let email = newApp.roles[key][i];
 
 				if (validateEmail(email)) {
-					//If the user exists, update the user's apps array with the new app's id.
+					// NOTE If the user exists, update the user's apps array with the new app's id.
 					if (await User.exists({ email: email })) {
 						const updatedUser = await User.findOne({ email: email });
 						console.log('User found: ', updatedUser);
@@ -51,7 +60,7 @@ router.post('/', async (req, res) => {
 						await updatedUser.save();
 						console.log('User updated successfully:', updatedUser);
 					}
-					//If the user does not exist, create a new user and add the app to their apps array.
+					// NOTE If the user does not exist, create a new user and add the app to their apps array.
 					else {
 						const newUser = await User.create({
 							email: email,
@@ -144,10 +153,10 @@ router.put('/:id', async (req, res) => {
 		//     }
 		//   }
 
-		await App.findByIdAndUpdate(id, update);
+		const updatedApp = await App.findByIdAndUpdate(id, update);
 		console.log(`App ${id} updated successfully`);
 
-		res.status(204).send();
+		res.status(200).json(updatedApp);
 	} catch (error) {
 		console.error('Error while updating app: ', error);
 
@@ -156,46 +165,41 @@ router.put('/:id', async (req, res) => {
 });
 
 // ROUTE cruD - delete an App
-// FIXME delete all app-scpViews when App deleted
 router.delete('/:id', async (req, res) => {
 	const id = req.params.id;
 
 	try {
-		// const deletedApp = await App.findByIdAndDelete(id);
-		// console.log('App deleted successfully', deletedApp);
-
-		// res.status(200).json(deletedApp);
-
-		// find an App by id
-		// store App.views
-		// map and delete all Views in App.views
-
 		const app = await App.findById(id);
-
-		if (!app) {
-			return res.status(404).send(`App ${id} not found`);
-		}
-
 		const appViews = app.views;
-
 		for (const viewId of appViews) {
-			await View.findByIdAndDelete(viewId);
+			try {
+				await View.findByIdAndDelete(viewId);
+			} catch (error) {
+				console.error('Error deleting a View: ', error);
+				return res.status(404).json({ message: `View ${viewId} not found` });
+			}
 		}
-
-		await app.delete();
-
-		// await App.findByIdAndDelete(id);
-		console.log(`App ${id} deleted successfully`);
-
-		//Remove the app id from the apps array of all users who have access to it.
-		const update = { $pull: { apps: id } };
-		await User.updateMany({ apps: id }, update);
-
-		res.status(204).send();
+		try {
+			// NOTE Remove the app id from the apps array of all users who have access to it.
+			const update = { $pull: { apps: id } };
+			await User.updateMany({ apps: id }, update);
+		} catch (error) {
+			console.error('Error updating Users: ', error);
+			return res
+				.status(404)
+				.json({ message: `Failed to update Users with App ${id}` });
+		}
+		try {
+			await app.delete();
+			console.log(`App ${id} deleted successfully`);
+			return res.status(204).send();
+		} catch (error) {
+			console.error('Error while deleting an App: ', error);
+			return res.status(500).json({ message: `Failed to delete App ${id}` });
+		}
 	} catch (error) {
-		console.error('Error while deleting app: ', error);
-
-		res.status(500).json({ message: `Failed to delete App ${id}` });
+		console.error('Error while finding an App: ', error);
+		return res.status(404).json({ message: `App ${id} not found` });
 	}
 });
 
