056cda21a356288c99add3803e12e0c264a8b685
mlee990125 <michael.j.lee@stonybrook.edu>
2023-04-30 02:39:27 -0400

    deleted addrow and deleterow because they are no longer needed

--- routes/googleapis.js
+++ routes/googleapis.js
@@ -35,6 +35,12 @@ function ensureAuthenticated(req, res, next) {
 }
 
 function extractSheetId(url) {
+  const match = /#gid=(\d+)/.exec(url);
+  return match ? match[1] : null;
+}
+
+
+function extractSpreadSheetId(url) {
   const regex = /spreadsheets\/d\/([a-zA-Z0-9-_]+)/;
   const match = url.match(regex);
 
@@ -80,7 +86,7 @@ async function getUserSheetsData(sheetId, sheetIndex, accessToken) {
 // ROUTE - get spreadsheet metadata
 router.post("/getmetadata", ensureAuthenticated, async (req, res) => {
   const { url } = req.body;
-  const spreadsheetId = extractSheetId(url);
+  const spreadsheetId = extractSpreadSheetId(url);
 
   const oauth2Client = new OAuth2();
   oauth2Client.setCredentials({ access_token: req.user.accessToken });
@@ -105,7 +111,7 @@ router.post("/loadsheet", ensureAuthenticated, async (req, res) => {
   const { name, url, sheetIndex } = req.body;
   try {
     const rows = await getUserSheetsData(
-      extractSheetId(url),
+      extractSpreadSheetId(url),
       sheetIndex,
       req.user.accessToken
     );
@@ -122,7 +128,7 @@ router.post("/loadsheet", ensureAuthenticated, async (req, res) => {
 // ROUTE - edit sheet values
 router.post("/updatesheet", ensureAuthenticated, async (req, res) => {
   const { name, url, range, values } = req.body;
-  const spreadsheetId = extractSheetId(url);
+  const spreadsheetId = extractSpreadSheetId(url);
   try {
     const response = await updateUserSheet(
       spreadsheetId,
@@ -140,119 +146,5 @@ router.post("/updatesheet", ensureAuthenticated, async (req, res) => {
   }
 });
 
-// ROUTE - add row to sheet
-router.post("/addrow", ensureAuthenticated, async (req, res) => {
-  const { url, sheetIndex, values } = req.body;
-  const spreadsheetId = extractSheetId(url);
-
-  const oauth2Client = new OAuth2();
-  oauth2Client.setCredentials({ access_token: req.user.accessToken });
-  const sheets = google.sheets({ version: "v4", auth: oauth2Client });
-
-  try {
-    // Get the total number of rows in the sheet
-    // const metadata = await sheets.spreadsheets.get({
-    //   spreadsheetId,
-    //   ranges: [sheetIndex],
-    //   includeGridData: false,
-    // });
-    const spreadsheetMetadata = await sheets.spreadsheets.get({
-      spreadsheetId,
-    });
-
-    // Find the sheet with the given name and get its ID
-    // const sheet = spreadsheetMetadata.data.sheets.find(
-    //   (s) => s.properties.title === sheetIndex
-    // );
-    // const sheetId = sheet.properties.sheetId;
-
-    res.send(spreadsheetMetadata);
-  } catch(error){
-    console.error(error);
-
-  }
-
-
-
-  //   const numRows = sheet.properties.gridProperties.rowCount;
-  //   const numCols = sheet.properties.gridProperties.columnCount;
-
-  //   // Add a new row at the end of the sheet
-  //   const insertResponse = await sheets.spreadsheets.batchUpdate({
-  //     spreadsheetId,
-  //     resource: {
-  //       requests: [
-  //         {
-  //           insertDimension: {
-  //             range: {
-  //               sheetId: sheetId,
-  //               dimension: "ROWS",
-  //               startIndex: numRows, // This will be the new last row
-  //               endIndex: numRows + 1,
-  //             },
-  //             inheritFromBefore: false,
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   });
-
-  //   // Fill in the values for the new row
-  //   const updateResponse = await sheets.spreadsheets.values.update({
-  //     spreadsheetId,
-  //     range: `${sheetIndex}!A${numRows + 1}:${columnToLetter(numCols)}${
-  //       numRows + 1
-  //     }`,
-  //     valueInputOption: "USER_ENTERED",
-  //     resource: {
-  //       values: values,
-  //     },
-  //   });
-
-  //   res.send({
-  //     insertResponse: insertResponse.data,
-  //     updateResponse: updateResponse.data,
-  //   });
-  // } catch (error) {
-  //   if (error.code === 403) {
-  //     res.status(403);
-  //   } else {
-  //     res.status(500).send(error);
-  //   }
-  // }
-});
-
-// router.post("/deleterow", ensureAuthenticated, async (req, res) => {
-//   const { url, sheetName, startIndex, endIndex } = req.body;
-//   const spreadsheetId = extractSheetId(url);
-
-//   const oauth2Client = new OAuth2();
-//   oauth2Client.setCredentials({ access_token: req.user.accessToken });
-//   const sheets = google.sheets({ version: "v4", auth: oauth2Client });
-
-//   try {
-//     const response = await sheets.spreadsheets.batchUpdate({
-//       spreadsheetId,
-//       resource: {
-//         requests: [
-//           {
-//             deleteDimension: {
-//               range: {
-//                 sheetId: sheetName,
-//                 dimension: "ROWS",
-//                 startIndex: startIndex,
-//                 endIndex: endIndex
-//               }
-//             }
-//           }
-//         ]
-//       }
-//     });
-//     res.send(response.data);
-//   } catch (error) {
-//     console.log("Error deleting rows from Google Sheet:", error);
-//     res.status(500).send(error);
-//   }
-// });
 
 module.exports = router;
